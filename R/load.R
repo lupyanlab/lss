fa3cf14de3a4b94b3d40f0d89c4af84b4d6acb4d
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-10-31 12:25:54 -0500

    Successfully load both LSS versions

--- R/load.R
+++ R/load.R
@@ -1,8 +1,21 @@
 library(dplyr)
+library(readr)
 
-load_LSS <- function(pattern = "LSS", header = "header_LSS.txt") {
-  header <- read.delim(header) %>% colnames
-  list.files("data", pattern = pattern, full.names = TRUE) %>%
-    lapply(read.delim, col.names = header, stringsAsFactors = FALSE) %>%
+load_LSS_participant <- function(name, col_names) {
+  read_tsv(name, col_names = col_names)
+}
+
+load_LSS <- function(pattern = "LSS", header = "header_LSS.txt", ...) {
+  col_names <- read_tsv(header) %>% colnames
+  list.files("data", pattern = pattern, full.names = TRUE, ...) %>%
+    lapply(load_LSS_participant, col_names = col_names) %>%
     bind_rows()
+}
+
+load_LSS_v1 <- function() {
+  load_LSS(pattern = "LSS1")
+}
+
+load_LSS_v2 <- function() {
+  load_LSS(pattern = "lss_v2", header = "header_LSS_v2.txt", ignore.case = TRUE)
 }
