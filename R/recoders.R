c8afee32cd249d8a206a57331e41cbd0e0de1126
Pierce Edmiston <pierce.edmiston@gmail.com>
2016-11-02 16:26:12 -0500

    Add SOA and cuePicMapping analyses

--- R/recoders.R
+++ R/recoders.R
@@ -12,3 +12,24 @@ recode_exp_name <- function(frame) {
   
   frame %>% left_join(exp_name_map)
 }
+
+recode_cue_pic_mapping <- function(frame) {
+  # Recode cuePicMapping variable in terms of the consistency
+  # of the mapping, e.g., cuePicMapping: fixed == reversed
+  # in that both are constant mappings (as opposed to random).
+  cue_pic_mapping_levels <- c("random", "fixed", "reversed")
+  cue_pic_mapping_map <- dplyr::data_frame(
+    cuePicMapping = cue_pic_mapping_levels,
+    mapping_consistency = c("random", "constant", "constant")
+  )
+  
+  mapping_consistency_levels <- c("random", "constant")
+  mapping_consistency_map <- dplyr::data_frame(
+    mapping_consistency = mapping_consistency_levels,
+    mapping_consistency_c = c(-0.5, 0.5),
+    mapping_consistency_label = factor(mapping_consistency_levels, levels = mapping_consistency_levels)
+  )
+  
+  left_join(cue_pic_mapping_map, mapping_consistency_map) %>%
+    left_join(frame, .)
+}
--- results.R
+++ results.R
@@ -11,8 +11,9 @@ source("R/tidy.R")
 source("R/recoders.R")
 
 LSS <- load_LSS_all() %>%
-  tidy_LSS %>%
-  recode_exp_name
+  tidy_LSS() %>%
+  recode_exp_name() %>%
+  recode_cue_pic_mapping()
 
 # ---- subjects
 subjects <- LSS %>%
@@ -26,17 +27,19 @@ subjects <- LSS %>%
     error_rank = rank(error, ties.method = "random")
   )
 
-rt_ylim_padding <- 50
+subj_id_size <- 2.2
+
+rt_ylim_padding <- 200
 ggplot(subjects, aes(x = rt_rank, y = rt, label = subjCode)) +
   geom_point(size = 1.0) +
-  geom_text(angle = 90, hjust = -0.1, size = 3) +
-  coord_cartesian(ylim = c(min(subjects$rt) - rt_ylim_padding, max(subjects$rt) + rt_ylim_padding))
+  geom_text(angle = 90, hjust = -0.1, size = subj_id_size) +
+  coord_cartesian(ylim = c(min(subjects$rt), max(subjects$rt) + rt_ylim_padding))
 
-error_ylim_padding <- 0.1
+error_ylim_padding <- 0.15
 ggplot(subjects, aes(x = error_rank, y = error, label = subjCode)) +
   geom_point(size = 1.0) +
-  geom_text(angle = 90, hjust = -0.1, size = 3) +
-  coord_cartesian(ylim = c(min(subjects$error) - error_ylim_padding, max(subjects$error) + error_ylim_padding))
+  geom_text(angle = 90, hjust = -0.1, size = subj_id_size) +
+  coord_cartesian(ylim = c(min(subjects$error), max(subjects$error) + error_ylim_padding))
 
 # ---- drop-outliers
 outliers <- c("LSS115")
@@ -54,7 +57,7 @@ rt_preds <- expand.grid(valid_cue = 0:1, exp_c = c(-0.5, 0.5)) %>%
 ggplot(LSS, aes(valid_cue, rt)) +
   geom_bar(stat = "summary", fun.y = "mean") +
   geom_pointrange(aes(ymin = rt - se, ymax = rt + se),
-                  data = rt_preds) +
+                  data = rt_preds, shape = 1) +
   coord_cartesian(ylim = c(300, 450)) +
   facet_wrap("exp_factor")
 
@@ -71,6 +74,46 @@ error_preds <- expand.grid(valid_cue = 0:1, exp_c = c(-0.5, 0.5)) %>%
 ggplot(LSS, aes(valid_cue, is_error)) +
   geom_bar(stat = "summary", fun.y = "mean") +
   geom_pointrange(aes(ymin = is_error - se, ymax = is_error + se),
-                  data = error_preds) +
+                  data = error_preds, shape = 1) +
   scale_y_continuous(labels = scales::percent) +
   facet_wrap("exp_factor")
+
+# ---- soa-plot
+ggplot(LSS, aes(factor(soa), rt, group = valid_cue)) +
+  geom_point(aes(color = factor(valid_cue)),
+             stat = "summary", fun.y = "mean") +
+  geom_smooth(aes(color = factor(valid_cue)), method = "lm") +
+  facet_wrap("exp_factor") +
+  theme(legend.position = "top")
+
+# ---- mapping-rt-mod
+mapping_rt_mod <- lmer(rt ~ mapping_consistency_c * valid_cue + (valid_cue|subjCode),
+                       data = LSS)
+
+mapping_rt_preds <- expand.grid(
+    mapping_consistency_c = c(-0.5, 0.5),
+    valid_cue = c(0, 1),
+    stringsAsFactors = FALSE
+  ) %>%
+  cbind(., predictSE(mapping_rt_mod, ., se = TRUE)) %>%
+  rename(rt = fit, se = se.fit) %>%
+  recode_cue_pic_mapping()
+
+# ---- mapping-rt-plot
+dodge_width <- position_dodge(width = 0.9)
+base_plot <- ggplot(LSS, aes(mapping_consistency, rt, group = valid_cue)) +
+  geom_bar(aes(fill = factor(valid_cue)),
+           stat = "summary", fun.y = "mean",
+           position = dodge_width) +
+  coord_cartesian(ylim = c(240, 460)) +
+  theme(legend.position = "top")
+
+base_plot +
+  facet_wrap("exp_factor")
+
+base_plot +
+  geom_pointrange(aes(ymin = rt - se, ymax = rt + se),
+                  data = mapping_rt_preds,
+                  position = dodge_width,
+                  shape = 1)
+
